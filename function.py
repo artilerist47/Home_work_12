913ebb89f52cd11517c0462cc955ee9f0e040e55
artilerist47 <artilerist.sorok.sedmoi@yandex.ru>
2022-06-26 03:21:14 +1000

    2 функция написана как однострочник

--- function.py
+++ function.py
@@ -7,16 +7,26 @@ def load_posts():
 
 
 def get_posts_by_word(word):
-    result = []
-    for post in load_posts():
-        if word.lower() in post["content"].lower():
-            result.append(post)
+    result = [post for post in load_posts() if word in post["content"].lower()]
+    # result = []
+    # for post in load_posts():
+    #     if word.lower() in post["content"].lower():
+    #         result.append(post)
     return result
 
 
 def add_post(post):
     posts = load_posts()
     posts.append(post)
-    with open("posts.json", "a", encoding="utf-8") as file:
+    # with open("posts.json", "a", encoding="utf-8") as file:
+    with open("posts.json", "w", encoding="utf-8") as file:
         json.dump(posts, file, ensure_ascii=False)
     return post
+
+
+
+
+
+
+
+
